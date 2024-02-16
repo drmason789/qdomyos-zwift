f2b273b7de5c6b437cfa60f91f11dbc9381bfedd
David Mason <drmason999@gmail.com>
2024-02-16 22:22:29 +0000

    Comments

--- tst/Devices/Bike/biketestdata.h
+++ tst/Devices/Bike/biketestdata.h
@@ -12,6 +12,11 @@ public:
 
 class BikeTestData : public BluetoothDeviceTestData {
 protected:
+    /**
+     * @brief Template function called from createInstance to create an instance of the device class.
+     * @param options
+     * @return
+     */
     virtual bike* doCreateInstance(const BikeOptions& options) =0;
 
 public:
@@ -25,6 +30,17 @@ public:
         return dynamic_cast<bike*>(detectedDevice)!=nullptr;
     }
 
-    bike* createInstance(const BikeOptions options) { return this->doCreateInstance(options); }
+    /**
+     * @brief (Potentially amongst other things) Calls a protected function to create an instance of the device class.
+     * @param options
+     * @return
+     */
+    bike* createInstance(const BikeOptions& options) {
+        // potentially validate or alter the options
+        auto result = this->doCreateInstance(options);
+
+        // potentially adjust the device object (e.g. connect signals and slots)
+        return result;
+    }
 };
 
