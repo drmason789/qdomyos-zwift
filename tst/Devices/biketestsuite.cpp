9a143bcf2f9e9ceec26f27ceb6c174aa0003362f
David Mason <drmason999@gmail.com>
2024-03-01 00:19:17 +0000

    Split up tests and improved reporting

--- tst/Devices/biketestsuite.cpp
+++ tst/Devices/biketestsuite.cpp
@@ -2,77 +2,149 @@
 #include "ErgInterface/bikeergfunctions.h"
 
 template<typename T>
-void BikeTestSuite<T>::SetUp() {}
+BikeTestSuite<T>::BikeTestSuite() : testSettings("Roberto Viola", "QDomyos-Zwift Testing") {}
 
 template<typename T>
-void BikeTestSuite<T>::test_powerFunctions() {
+void BikeTestSuite<T>::SetUp() {
     BikeOptions options;
-    auto device = std::unique_ptr<bike>(this->typeParam.createInstance(options));
-    auto erg = std::unique_ptr<erginterface>(new bikeergfunctions(device.get()));
+    this->device =this->typeParam.createInstance(options);
+    this->ergInterface = new bikeergfunctions(this->device);
 
-    const uint32_t maxRPM = erg->getMaxCadence().value_or(255);
-    const uint32_t minRPM = erg->getMinCadence().value_or(1);
-    const resistance_t maxResistance = erg->getMaxResistance().value_or(255);
-    const resistance_t minResistance = erg->getMinResistance().value_or(0);
+    this->maxRPM = this->ergInterface->getMaxCadence().value_or(120);
+    this->minRPM = this->ergInterface->getMinCadence().value_or(1);
+    this->maxResistance = this->ergInterface->getMaxResistance().value_or(255);
+    this->minResistance = this->ergInterface->getMinResistance().value_or(0);
 
+}
+
+template<typename T>
+void BikeTestSuite<T>::TearDown() {
+    if(this->device) delete this->device;
+    if(this->ergInterface) delete this->ergInterface;
+    this->device = nullptr;
+    this->ergInterface = nullptr;
+}
+
+template<typename T>
+void BikeTestSuite<T>::test_powerFunctions_minResistance() {
+    const auto erg = this->ergInterface;
     uint16_t p0, p1;
     resistance_t r0, r1;
     QStringList errors;
-    QString  powerBeyondResistanceLimit = QStringLiteral("Power at C:%1 RPM not confined at %6 resistance (R:%2, P:%3W), (R:%4, P:%5W)");
+    QString  powerBeyondResistanceLimit = QStringLiteral("Power at C:%1 RPM not bounded at %6 resistance (R:%2, P:%3W), (R:%4, P:%5W)");
+
+    if(!erg->getMinResistance().has_value())
+    {
+        GTEST_SKIP() << "No minimum resistance defined";
+    }
 
     // traverse the cadence edges checking the power is clipped to the values for the max and min resistance
     for(uint32_t cadenceRPM=minRPM; cadenceRPM<=maxRPM; cadenceRPM++)
     {
-        if(erg->getMaxResistance().has_value()) {
+        r0 = minResistance;
+        r1 = minResistance-1;
+        p0 = erg->getPower(cadenceRPM, r0);
+        p1 = erg->getPower(cadenceRPM, r1);
 
-            r0 = maxResistance;
-            r1 = maxResistance+1;
-            p0 = erg->getPower(cadenceRPM, r0);
-            p1 = erg->getPower(cadenceRPM, r1);
+        if(p0 != p1)
+            errors.append(powerBeyondResistanceLimit.arg(cadenceRPM).arg(r0).arg(p0).arg(r1).arg(p1).arg("minimum"));
+    }
 
-            if(p0 != p1)
-                errors.append(powerBeyondResistanceLimit.arg(cadenceRPM).arg(r0).arg(p0).arg(r1).arg(p1).arg("maximum"));
-        }
+    ASSERT_TRUE(errors.empty()) << errors.join('\n').toStdString();
+}
 
-        if(erg->getMinResistance().has_value()) {
-            r0 = minResistance;
-            r1 = minResistance-1;
-            p0 = erg->getPower(cadenceRPM, r0);
-            p1 = erg->getPower(cadenceRPM, r1);
 
-            if(p0 != p1)
-                errors.append(powerBeyondResistanceLimit.arg(cadenceRPM).arg(r0).arg(p0).arg(r1).arg(p1).arg("minimum"));
-        }
+template<typename T>
+void BikeTestSuite<T>::test_powerFunctions_maxResistance() {
+    const auto erg = this->ergInterface;
+    uint16_t p0, p1;
+    resistance_t r0, r1;
+    QStringList errors;
+    QString  powerBeyondResistanceLimit = QStringLiteral("Power at C:%1 RPM not bounded at %6 resistance (R:%2, P:%3W), (R:%4, P:%5W)");
+
+    if(!erg->getMaxResistance().has_value())
+    {
+        GTEST_SKIP() << "No maximum resistance defined.";
+    }
+
+    // traverse the cadence edges checking the power is clipped to the values for the max and min resistance
+    for(uint32_t cadenceRPM=minRPM; cadenceRPM<=maxRPM; cadenceRPM++)
+    {
+        r0 = maxResistance;
+        r1 = maxResistance+1;
+        p0 = erg->getPower(cadenceRPM, r0);
+        p1 = erg->getPower(cadenceRPM, r1);
+
+        if(p0 != p1)
+            errors.append(powerBeyondResistanceLimit.arg(cadenceRPM).arg(r0).arg(p0).arg(r1).arg(p1).arg("maximum"));
+    }
+
+    ASSERT_TRUE(errors.empty()) << errors.join('\n').toStdString();
+}
+
+template<typename T>
+void BikeTestSuite<T>::test_powerFunctions_minCadence() {
+    const auto erg = this->ergInterface;
+    uint16_t p0, p1;
+    QStringList errors;
+
+    if(!erg->getMinCadence().has_value())
+    {
+        GTEST_SKIP() << "No minimum cadence defined.";
     }
 
     // traverse the resistance edge checking the power is clipped to the values for the max and min cadence
 
-    QString  powerBeyondCadenceLimit = QStringLiteral("Power at R:%1 not confined at %6 cadence (C:%2 RPM, P:%3W), (C:%4 RPM, P:%5W)");
-
+    QString  powerBeyondCadenceLimit = QStringLiteral("Power at R:%1 not bounded at %6 cadence (C:%2 RPM, P:%3W), (C:%4 RPM, P:%5W)");
 
     for(resistance_t r=minResistance; r<=maxResistance; r++)
     {
-        if(erg->getMinCadence().has_value()) {
-            const int32_t c0 = minRPM, c1=minRPM-1;
-            p0 = erg->getPower(c0, r);
-            p1 = erg->getPower(c1, r);
+        const int32_t c0 = minRPM, c1=minRPM-1;
+        p0 = erg->getPower(c0, r);
+        p1 = erg->getPower(c1, r);
 
-            if(p0!=p1)
-                errors.append(powerBeyondCadenceLimit.arg(r).arg(c0).arg(p0).arg(c1).arg(p1).arg("minimum"));
-        }
-
-        if(erg->getMaxCadence().has_value()) {
-            const int32_t c0 = maxRPM, c1=maxRPM+1;
-            p0 = erg->getPower(c0, r);
-            p1 = erg->getPower(c1, r);
-
-            if(p0!=p1)
-                errors.append(powerBeyondCadenceLimit.arg(r).arg(c0).arg(p0).arg(c1).arg(p1).arg("maximum"));
-        }
+        if(p0!=p1)
+            errors.append(powerBeyondCadenceLimit.arg(r).arg(c0).arg(p0).arg(c1).arg(p1).arg("minimum"));
     }
 
+    ASSERT_TRUE(errors.empty()) << errors.join('\n').toStdString();
+}
+
+template<typename T>
+void BikeTestSuite<T>::test_powerFunctions_maxCadence() {
+    const auto erg = this->ergInterface;
+    uint16_t p0, p1;
+    QStringList errors;
+
+    if(!erg->getMaxCadence().has_value())
+    {
+        GTEST_SKIP() << "No maximum cadence defined";
+    }
+
+    // traverse the resistance edge checking the power is clipped to the values for the max and min cadence
+
+    QString  powerBeyondCadenceLimit = QStringLiteral("Power at R:%1 not bounded at %6 cadence (C:%2 RPM, P:%3W), (C:%4 RPM, P:%5W)");
+
+    for(resistance_t r=minResistance; r<=maxResistance; r++)
+    {
+        const int32_t c0 = maxRPM, c1=maxRPM+1;
+        p0 = erg->getPower(c0, r);
+        p1 = erg->getPower(c1, r);
+
+        if(p0!=p1)
+            errors.append(powerBeyondCadenceLimit.arg(r).arg(c0).arg(p0).arg(c1).arg(p1).arg("maximum"));
+    }
+
+    ASSERT_TRUE(errors.empty()) << errors.join('\n').toStdString();
+}
+
+template<typename T>
+void BikeTestSuite<T>::test_powerFunctions_resistancePowerConversion() {
+    const auto erg = this->ergInterface;
+    QStringList errors;
+
     // test inverses
-    QString unexpectedResistance=QStringLiteral("Expected resistance R:%1 to achieve P:%2 W at C:%3 RPM, but got R:%4");
+    QString unexpectedResistance=QStringLiteral("P(C:%3, R:%1)=%2 but R(C:%3, P:%2)=%4 and P(C:%3, R:%4)=%5");
     for(uint32_t cadenceRPM=minRPM; cadenceRPM<=maxRPM; cadenceRPM++)
     {
         uint16_t lastPower=0xFFFF;
@@ -80,23 +152,22 @@ void BikeTestSuite<T>::test_powerFunctions() {
         {
             uint16_t power = erg->getPower(cadenceRPM, r);
 
+            // if this resistance reaches a new power level, check the inverse
             if(power!=lastPower)
             {
                 lastPower = power;
                 resistance_t resistance = erg->getResistance(cadenceRPM, power);
 
-                if(r!=resistance)
-                    errors.append(unexpectedResistance.arg(resistance).arg(power).arg(cadenceRPM).arg(r));
+                if(r!=resistance) {
+                    uint16_t newPower = erg->getPower(cadenceRPM, resistance);
+
+                    errors.append(unexpectedResistance.arg(r).arg(power).arg(cadenceRPM).arg(resistance).arg(newPower));
+                }
 
             }
 
         }
     }
 
-    if(!errors.empty()){
-        auto errorMessage = errors.join('\n').toStdString();
-
-        FAIL() << errorMessage;
-    }
-
+    ASSERT_TRUE(errors.empty()) << errors.join('\n').toStdString();
 }
--- tst/Devices/biketestsuite.h
+++ tst/Devices/biketestsuite.h
@@ -2,37 +2,64 @@
 
 #include "gtest/gtest.h"
 #include "devices.h"
+#include "ErgInterface/erginterface.h"
 
 #include "Tools/testsettings.h"
 
 template <typename T>
 class BikeTestSuite : public testing::Test {
+private:
+    bike * device = nullptr;
 
 protected:
     T typeParam;
+
+    uint32_t maxRPM, minRPM;
+    resistance_t maxResistance, minResistance;
+
+    erginterface * ergInterface = nullptr;
 
     /**
      * @brief Manages the QSettings used during the tests, separate from QSettings stored in the system generally.
      */
     TestSettings testSettings;
 public:
-    BikeTestSuite() : testSettings("Roberto Viola", "QDomyos-Zwift Testing") {}
+    BikeTestSuite();
 
     // Sets up the test fixture.
     void SetUp() override;
 
     // Tears down the test fixture.
-    // virtual void TearDown();
+    virtual void TearDown() override;
 
+    void test_powerFunctions_minCadence();
+    void test_powerFunctions_minResistance();
+    void test_powerFunctions_maxCadence();
+    void test_powerFunctions_maxResistance();
+    void test_powerFunctions_resistancePowerConversion();
 
-    void test_powerFunctions();
 };
 
 
 TYPED_TEST_SUITE(BikeTestSuite, BikeTestDataTypes);
 
-TYPED_TEST(BikeTestSuite, TestPowerFunctions) {
-    this->test_powerFunctions();
+TYPED_TEST(BikeTestSuite, TestPowerFunctionsMinCadence) {
+    this->test_powerFunctions_minCadence();
 }
 
+TYPED_TEST(BikeTestSuite, TestPowerFunctionsMaxCadence) {
+    this->test_powerFunctions_maxCadence();
+}
+
+TYPED_TEST(BikeTestSuite, TestPowerFunctionsMinResistance) {
+    this->test_powerFunctions_minResistance();
+}
+
+TYPED_TEST(BikeTestSuite, TestPowerFunctionsMaxResistance) {
+    this->test_powerFunctions_maxResistance();
+}
+
+TYPED_TEST(BikeTestSuite, TestPowerFunctionsResistancePowerConversion) {
+    this->test_powerFunctions_resistancePowerConversion();
+}
 
